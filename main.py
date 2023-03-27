349031db0255bf4bc0ee4f5ec328f657e637af44
unset <italo.gamer.3669@gmail.com>
2023-03-27 18:13:45 -0300

    python

---
+++ main.py
@@ -0,0 +1,22 @@
+def max(num1,num2):
+ if(num1 > num2):
+   
+print('o maior valor é:',num1)
+else:
+print('o maior valor é:',num2)
+
+max(5,6)
+max(2,1)
+max(7,7)
+
+def numeromultiplos(num1,num2):
+if num1%num2==8:
+  return True
+else:
+  return False
+  numeromultiplo(35,7)
+
+  def areadoquadrado(lado:)
+   area =lado*lado
+print=area
+areadoquadrado()
